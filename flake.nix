5aed4cbb9d068cf312ae9a995c302f3389c5a250
heinwol <heinvvol@gmail.com>
2023-11-07 12:50:26 +0300

    updated to v1.5.0

--- flake.nix
+++ flake.nix
@@ -3,25 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, poetry2nix }:
     let
 
       system = "x86_64-linux";
 
       pkgs = nixpkgs.legacyPackages.${system};
+      poetry2nixLib = (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; });
 
       src = pkgs.fetchFromGitHub {
         owner = "ssine";
         repo = "pptx2md";
-        rev = "bb9762fcb537190cee3e71d18b56da07fdbbb3eb";
-        hash = "sha256-sw1Io7nnohJvhwglrR8NjKi+OJ8K9QTfM0/93VZm2TY=";
+        rev = "baa1a551b7c451ec7b8a76b4979448c5c742f44b";
+        hash = "sha256-oU7cetUA3M+0g5waIz3TTmen7uILsF8idjmRAF3TVxM=";
       };
 
-      pptx2md = pkgs.poetry2nix.mkPoetryApplication {
+      pptx2md = poetry2nixLib.mkPoetryApplication {
         projectDir = src;
-        overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend
+        overrides = poetry2nixLib.defaultPoetryOverrides.extend
           (self: super: {
             python-pptx = super.python-pptx.overridePythonAttrs
               (
